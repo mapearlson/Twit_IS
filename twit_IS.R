3ca1ea9e6edf01ab27de39074f00b92a62cc2566
Michael Pearlson <mapearlson@gmail.com>
2015-03-12 13:35:05 +0400

    not much, using different sources for code

--- twit_IS.R
+++ twit_IS.R
@@ -1,1 +1,32 @@
-bjkl
+library(twitteR)
+# retrieve the first 200 tweets (or all tweets if fewer than 200) from the
+# user timeline of @rdatammining
+rdmTweets <- userTimeline("rdatamining", n=200)
+(nDocs <- length(rdmTweets))
+rdmTweets[11:15]
+
+for (i in 11:15) {
+  +   cat(paste("[[", i, "]] ", sep=""))
+  + writeLines(strwrap(rdmTweets[[i]]$getText(), width=73)) +}
+
+# convert tweets to a data frame
+df <- do.call("rbind", lapply(rdmTweets, as.data.frame))
+dim(df)
+
+library(tm)
+# build a corpus, and specify the source to be character vectors
+myCorpus <- Corpus(VectorSource(df$text))
+# convert to lower case
+myCorpus <- tm_map(myCorpus, tolower)
+# remove punctuation
+myCorpus <- tm_map(myCorpus, removePunctuation)
+# remove numbers
+myCorpus <- tm_map(myCorpus, removeNumbers)
+# remove URLs
+removeURL <- function(x) gsub("http[[:alnum:]]*", "", x)
+myCorpus <- tm_map(myCorpus, removeURL)
+# add two extra stop words: "available" and "via"
+myStopwords <- c(stopwords('english'), "available", "via") > # remove "r" and "big" from stopwords
+myStopwords <- setdiff(myStopwords, c("r", "big"))
+> # remove stopwords from corpus
+  > myCorpus <- tm_map(myCorpus, removeWords, myStopwords)
