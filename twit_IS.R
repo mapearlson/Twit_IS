253a48ea9689910d008874238ba96d5697be38ce
Michael Pearlson <mapearlson@gmail.com>
2015-03-14 11:55:27 +0400

    source

--- twit_IS.R
+++ twit_IS.R
@@ -6,8 +6,8 @@ rdmTweets <- userTimeline("rdatamining", n=200)
 rdmTweets[11:15]
 
 for (i in 11:15) {
-  +   cat(paste("[[", i, "]] ", sep=""))
-  + writeLines(strwrap(rdmTweets[[i]]$getText(), width=73)) +}
+  cat(paste("[[", i, "]] ", sep=""))
+  writeLines(strwrap(rdmTweets[[i]]$getText(), width=73)) +}
 
 # convert tweets to a data frame
 df <- do.call("rbind", lapply(rdmTweets, as.data.frame))
@@ -26,7 +26,26 @@ myCorpus <- tm_map(myCorpus, removeNumbers)
 removeURL <- function(x) gsub("http[[:alnum:]]*", "", x)
 myCorpus <- tm_map(myCorpus, removeURL)
 # add two extra stop words: "available" and "via"
-myStopwords <- c(stopwords('english'), "available", "via") > # remove "r" and "big" from stopwords
+myStopwords <- c(stopwords('english'), "available", "via")
+# remove "r" and "big" from stopwords
 myStopwords <- setdiff(myStopwords, c("r", "big"))
 # remove stopwords from corpus
 myCorpus <- tm_map(myCorpus, removeWords, myStopwords)
+
+# keep a copy of corpus to use later as a dictionary for stem completion
+myCorpusCopy <- myCorpus
+# stem words
+myCorpus <- tm_map(myCorpus, stemDocument)
+# inspect documents (tweets) numbered 11 to 15
+# inspect(myCorpus[11:15])
+# The code below is used for to make text fit for paper width
+for (i in 11:15) {
+    cat(paste("[[", i, "]] ", sep=""))
+    writeLines(strwrap(myCorpus[[i]], width=73)) 
+}
+
+inspect(myCorpus[11:15])
+
+# stem completion
+myCorpus <- tm_map(myCorpus, stemCompletion, dictionary=myCorpusCopy)
+
